18c6a4b51043bc171738228b63667680cfc8074a
areuz <marekistok@gmail.com>
2017-09-24 00:18:18 +0200

    Fixed flipping with the touchscreen Still need to fix the on screen keyboard - focus problem

--- scripts/scr_flip.sh
+++ scripts/scr_flip.sh
@@ -5,7 +5,9 @@ STATUS="$(xrandr | grep "$MONITOR" | awk -F' ' '{print $5}')";
 
 if [ "$STATUS" == "inverted" ]; then 
       xrandr -o 0;
-      killall matchbox-keyboard;
+      ##killall matchbox-keyboard;
+      xinput set-prop 'ELAN22CA:00 04F3:22CA' 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 1;
 else  xrandr -o 2;
-      matchbox-keyboard &
+      ##matchbox-keyboard &
+      xinput set-prop 'ELAN22CA:00 04F3:22CA' 'Coordinate Transformation Matrix' -1 0 1 0 -1 1 0 0 1;
 fi;
