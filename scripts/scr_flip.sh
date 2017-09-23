c0c2c49894b53b55e62c1448e021c82ccd61acb2
areuz <marekistok@gmail.com>
2017-09-23 23:57:25 +0200

    Complete flip script --add transformation matrix

--- scripts/scr_flip.sh
+++ scripts/scr_flip.sh
@@ -5,5 +5,7 @@ STATUS="$(xrandr | grep "$MONITOR" | awk -F' ' '{print $5}')";
 
 if [ "$STATUS" == "inverted" ]; then 
       xrandr -o 0;
+      killall matchbox-keyboard;
 else  xrandr -o 2;
+      matchbox-keyboard &
 fi;
