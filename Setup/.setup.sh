e61f963ef3ad633b8ef018e2db2a392b76bd880e
areuz <marekistok@gmail.com>
2017-10-05 18:56:34 +0200

    Fixed networking resume script

--- Setup/.setup.sh
+++ Setup/.setup.sh
@@ -57,7 +57,7 @@ ln_replace "/etc/environment" "$CONF/System/environment";
 ln_replace "/etc/systemd/logind.conf" "$CONF/System/logind.conf";
 ln_replace "/etc/modprobe.d/alsa-base.conf" "$CONF/System/alsa-base.conf";
 ln_replace "/etc/vconsole.conf" "/$CONF/System/vconsole.conf";
-ln_replace "/etc/systemd/system/connman-resume.service" "$CONF/System/connman/connman-resume.service";
+ln_replace "/etc/systemd/system/net-resume.service" "$CONF/System/connman/net-resume.service";
 ln_replace "/etc/udev/rules.d" "$CONF/System/rules.d";
 
 
