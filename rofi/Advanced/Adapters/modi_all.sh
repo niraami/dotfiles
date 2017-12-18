26b35438111e997e2911104fc1b9c708b25560ec
areuz <marekistok@gmail.com>
2017-12-18 18:39:54 +0100

    Fixed adapter menu columns

--- rofi/Advanced/Adapters/modi_all.sh
+++ rofi/Advanced/Adapters/modi_all.sh
@@ -4,13 +4,20 @@ source "$THIS/inc.sh";
 
 if [ $# -eq 0 ]; then
   for ((i=0; i<$n_adapter; i++)); do
-    c_power="$( if [[ ${power[$i]} == "True" ]]; then
-      echo "$fgg"ON"$fgg_";  else  echo "$fgr"OFF"$fgr_"; fi)";
-    c_conn="$( if [[ ${conn[$i]} == "True" ]]; then
-      echo "$fgg"Online"$fgg_";  else  echo "$fgr"OFF"$fgr_"; fi)";
-    c_teth="$( if [[ ${teth[$i]} == "True" ]]; then
-      echo "$fgg"Tethering"$fgg_";  else  echo "$fgr"OFF"$fgr_"; fi)";
-
-    echo "${type[$i]}^^^$c_power^$c_conn^$c_teth" | column -s\^ -t;
+    c_power+=("$( if [[ ${power[$i]} == "True" ]]; then
+      echo "$fgg"ON"$fgg_";  else  echo "$fgr"OFF"$fgr_"; fi)");
+    c_conn+=("$( if [[ ${conn[$i]} == "True" ]]; then
+      echo "$fgg"Online"$fgg_";  else  echo "$fgr"Offline"$fgr_"; fi)");
+    c_teth+=("$( if [[ ${teth[$i]} == "True" ]]; then
+      echo "$fgg"Tethering"$fgg_";  else  echo "$fgr"OFF"$fgr_"; fi)");
   done;
 fi;
+
+out="";
+for ((i=0; i<$n_adapter; i++)); do
+  out+="${type[$i]}^^^${c_power[$i]}^${c_conn[$i]}^${c_teth[$i]}\n";
+done;
+
+echo "$( echo -e "$out" | column -s\^ -t )";
+
+
--- rofi/Advanced/Adapters/modi_connected.sh
+++ rofi/Advanced/Adapters/modi_connected.sh
@@ -4,15 +4,21 @@ source "$THIS/inc.sh";
 
 if [ $# -eq 0 ]; then
   for ((i=0; i<$n_adapter; i++)); do
-    if [[ ${conn[$i]} != "True" ]]; then break; fi;
-
-    c_power="$( if [[ ${power[$i]} == "True" ]]; then
-      echo "$fgg"ON"$fgg_";  else  echo "$fgr"OFF"$fgr_"; fi)";
-    c_conn="$( if [[ ${conn[$i]} == "True" ]]; then
-      echo "$fgg"Online"$fgg_";  else  echo "$fgr"OFF"$fgr_"; fi)";
-    c_teth="$( if [[ ${teth[$i]} == "True" ]]; then
-      echo "$fgg"Tethering"$fgg_";  else  echo "$fgr"OFF"$fgr_"; fi)";
-
-    echo "${type[$i]}^^^$c_power^$c_conn^$c_teth" | column -s\^ -t;
+    if [[ ${conn[$i]} == "True" ]]; then
+      c_type+=("${type[$i]}");
+      c_power+=("$( if [[ ${power[$i]} == "True" ]]; then
+        echo "$fgg"ON"$fgg_";  else  echo "$fgr"OFF"$fgr_"; fi)");
+      c_conn+=("$( if [[ ${conn[$i]} == "True" ]]; then
+        echo "$fgg"Online"$fgg_";  else  echo "$fgr"Offline"$fgr_"; fi)");
+      c_teth+=("$( if [[ ${teth[$i]} == "True" ]]; then
+        echo "$fgg"Tethering"$fgg_";  else  echo "$fgr"OFF"$fgr_"; fi)");
+    fi;
   done;
 fi;
+
+out="";
+for ((i=0; i<${#c_type[@]}; i++)); do
+  out+="${c_type[$i]}^^^${c_power[$i]}^${c_conn[$i]}^${c_teth[$i]}\n";
+done;
+
+echo "$( echo -e "$out" | column -s\^ -t )";
