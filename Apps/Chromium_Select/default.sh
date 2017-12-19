7ae296c7d8382a8f93415931b58b1fcb93e4b8c8
areuz <marekistok@gmail.com>
2017-12-19 01:00:58 +0100

    Sync and update

--- Apps/Chromium_Select/default.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-#PRIORITY 5
-
-chromium
--- Apps/Chromium_Select/i2p_proxy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#PRIORITY 9
-
-dir="/tmp/chromium_i2p/";
-if [ ! -d "$dir" ]; then mkdir "$dir"; fi;
-
-chromium http://127.0.0.1:7657/home --incognito --user-data-dir="$dir" --proxy-server=127.0.0.1:4444 --proxy-bypass-list="127.0.0.1";
