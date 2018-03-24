c84e6feee425ae880306e67a07781cd9c3b7a41d
areuz <marekistok@gmail.com>
2018-03-24 12:22:16 +0100

    Switched to variable for browser - for faster changes if needed

--- rofi/chromium.sh
+++ rofi/chromium.sh
@@ -5,6 +5,8 @@ incognito_dir="/tmp/chromium_incognito/";
 tor_dir="/tmp/chromium_tor/";
 i2p_dir="/tmp/chromium_i2p/";
 
+browser='chromium';
+
 Opts=();
 Opts+=("Normal");
 Opts+=("Incognito");
@@ -17,21 +19,21 @@ if [ $# -eq 0 ]; then
 elif [[ "${Opts[@]}" =~ "$1" ]]; then
   case "$1" in
     "Normal")
-      google-chrome-stable > /dev/null 2>&1 &
+      $browser > /dev/null 2>&1 &
       ;;
 
     "Incognito")
       if [ -d "$incognito_dir" ]; then rm -rf "$incognito_dir"; fi;
       mkdir "$incognito_dir";
 
-      google-chrome-stable --incognito --user-data-dir="$incognito_dir" > /dev/null 2>&1 &
+      $browser --incognito --user-data-dir="$incognito_dir" > /dev/null 2>&1 &
       ;;
 
     "Tor")
       if [ -d "$tor_dir" ]; then rm -rf "$tor_dir"; fi;
       mkdir "$tor_dir";
 
-      google-chrome-stable --incognito \
+      $browser --incognito \
         --user-data-dir="$tor_dir" \
         --proxy-server="socks5://127.0.0.1:9050" \
         --host-resolver-rules="MAP * 0.0.0.0 , EXCLUDE myproxy" > /dev/null 2>&1 &
@@ -41,7 +43,7 @@ elif [[ "${Opts[@]}" =~ "$1" ]]; then
       if [ -d "$i2p_dir" ]; then rm -rf "$i2p_dir"; fi;
       mkdir "$i2p_dir";
 
-      google-chrome-stable http://127.0.0.1:7657/home --incognito \
+      $browser http://127.0.0.1:7657/home --incognito \
         --user-data-dir="$i2p_dir" \
         --proxy-server=127.0.0.1:4444 \
         --proxy-bypass-list="127.0.0.1" > /dev/null 2>&1 &
